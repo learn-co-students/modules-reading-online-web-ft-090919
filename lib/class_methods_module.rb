b52ffbc308577cc0471d6dbe6d0811f284ff91fb
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 20:57:57 +0000

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,18 @@
+module MetaDancing
+  
+  def metadata
+    "This class produces objects that love to dance."
+  end 
+end
+
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
+
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
+
